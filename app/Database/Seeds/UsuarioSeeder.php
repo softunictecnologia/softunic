848c698b4aa3804016865c42a0cf2586d1ccf979
SoftUnic <44954092+softunictecnologia@users.noreply.github.com>
2025-09-16 14:32:39 -0300

    Monstando DB

---
+++ app/Database/Seeds/UsuarioSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UsuarioSeeder extends Seeder
+{
+    public function run()
+    {
+        $usuarioModel = new \App\Models\UsuarioModel;
+
+        $usuario = [
+            'nome'     => 'Administrador do Sistema',
+            'email'    => 'adm@adm.com.br',
+            'telefone' => '(49) 3558-0701',
+        ];
+
+        $usuarioModel->protect(false)->insert($usuario);
+
+        $usuario = [
+            'nome'     => 'Daniel Pauly',
+            'email'    => 'danielpaulysc@gmail.com',
+            'telefone' => '(49) 98914-8914',
+        ];
+
+        $usuarioModel->protect(false)->insert($usuario);  
+        
+        dd($usuarioModel->errors());
+    }
+}
