c44c0b54fa9ddb37172d00d2031c1884427becba
SoftUnic Tecnologia <44954092+softunictecnologia@users.noreply.github.com>
2025-09-24 17:27:53 -0300

    9

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Libraries\Autenticacao;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Login extends BaseController
@@ -14,5 +15,29 @@ class Login extends BaseController
         ];
 
         return view('Login/novo', $data);
+    }
+
+    public function criar(){
+        if ($this->request->getMethod() === 'POST') {
+            $email = $this->request->getPost('email');
+            $password = $this->request->getPost('password');
+
+            $autenticacao = service('autenticacao');
+
+            if ($autenticacao->login($email, $password)) {
+                $usuario = $autenticacao->pegaUsuarioLogado();
+                dd($usuario);
+            } else {
+                return redirect()->back()->with('atencao', 'Não encontramos suas credenciais de acesso.');
+            }
+            
+        } else {
+            return redirect()->back();
+        }
+    }
+
+    public function logout() {
+        service('autenticacao')->logout();
+        return redirect()->to(site_url('login'));
     }
 }
